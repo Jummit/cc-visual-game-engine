5115d3a29c4461cdc37d9d4a841fe970af0e9d28
Jummit <jummit@web.de>
2018-11-28 12:26:23 +0100

    renderLine and printCenter function in utils

--- utils.lua
+++ utils.lua
@@ -4,6 +4,10 @@ function utils.renderBox(x, y, w, h, color)
   paintutils.drawFilledBox(x, y, w + x - 1, h + y - 1, color)
 end
 
+function utils.renderLine(x, y, dx, dy, color)
+  paintutils.drawLine(x, y, x + dx - 1, y + dy - 1, color)
+end
+
 function utils.pointInBox(x, y, w, h, px, py)
   return px >= x and py >= y and px < x + w and py < y + h
 end
@@ -12,6 +16,13 @@ function utils.clearTable(t)
   for k, v in pairs(t) do
     t[k] = nil
   end
+end
+
+function utils.printCenter(x, y, w, h, text)
+  term.setCursorPos(
+      x + w - #text * 1.5 + 1,
+      y + h / 2)
+  term.write(text)
 end
 
 return utils
