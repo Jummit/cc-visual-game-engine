534f735b3e472c4a9304c28d056d6d65799aea5b
Jummit <jummit@web.de>
2018-11-29 13:46:59 +0100

    moving list items works

--- moveButtons.lua
+++ moveButtons.lua
@@ -1,4 +1,19 @@
+local utils = require "utils"
 local newButton = require "button"
+
+local function moveListItems(t, f)
+  local items = utils.copyTable(t.list.items)
+  local selected = t.list.selected
+  local selectedItem = items[selected]
+  if selectedItem then
+    table.remove(items, selected)
+    utils.clearTable(t.list.items)
+
+    for n, item in ipairs(items) do
+      f(n, item, selected, selectedItem)
+    end
+  end
+end
 
 return function(t)
   local up = newButton{
@@ -8,7 +23,15 @@ return function(t)
     color = colors.gray, clickedColor = colors.cyan,
     labelColor = colors.white,
     onClick = function()
-
+      if t.list.selected > 1 then
+        moveListItems(t, function(n, item, selected, selectedItem)
+              if n == selected - 1 then
+                table.insert(t.list.items, selectedItem)
+              end
+              table.insert(t.list.items, item)
+            end)
+        t.list:select(t.list.selected - 1)
+      end
     end
   }
   local down = newButton{
@@ -18,7 +41,15 @@ return function(t)
     color = colors.gray, clickedColor = colors.cyan,
     labelColor = colors.white,
     onClick = function()
-
+      if t.list.selected < #t.list.items then
+        moveListItems(t, function(n, item, selected, selectedItem)
+              table.insert(t.list.items, item)
+              if n == selected then
+                table.insert(t.list.items, selectedItem)
+              end
+            end)
+        t.list:select(t.list.selected + 1)
+      end
     end
   }
 
