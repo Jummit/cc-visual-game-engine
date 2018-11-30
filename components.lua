9cf315903b10bf1c639755e6040865ac690a0b50
Jummit <jummit@web.de>
2018-11-30 12:40:57 +0100

    added sprite editor

--- components.lua
+++ components.lua
@@ -1,4 +1,7 @@
 local components = {}
+local newButton = require "button"
+local utils = require "utils"
+local colorRadialMenu = require "colorRadialMenu"
 
 local template = {
   args = {
@@ -50,29 +53,42 @@ components.pos = {
 
 components.sprite = {
   args = {
-    texture = {
-        " 3333 ",
-        "3b33b3",
-        "333333",
-        "3bbbb3",
-        " 3333 "},
+    texture = {}
   },
 
   render = function(self)
-    for y = 1, #self.texture do
-      local line = self.texture[y]
-      for x = 1, #line do
-        local char = string.sub(line, x, x)
-        if type(char) == "string" and char ~= " " then
-          term.setCursorPos(self.x + x, self.y + y)
-          term.blit(char, char, char)
-        end
+    for x, row in pairs(self.texture) do
+      for y, color in pairs(row) do
+        paintutils.drawPixel(self.x + x - 1, self.y + y - 1, color)
       end
+    end
+
+    utils.renderText(self.x, self.y, "+", colors.lightGray, colors.white)
+    if self.showTools then
+      colorRadialMenu.render(self.clickedX, self.clickedY)
     end
   end,
   update = function(self, event, var1, var2, var3)
   end,
   editor = function(self, event, var1, var2, var3)
+    if event == "mouse_click" or event == "mouse_drag" then
+      if var1 == 2 then
+        self.showTools = true
+        self.clickedX, self.clickedY = var2, var3
+      elseif self.showTools then
+        local color = colorRadialMenu.update(self.clickedX, self.clickedY, event, var1, var2, var3)
+        self.showTools = false
+        self.drawColor = color
+      elseif self.drawColor then
+        local x = var2 - self.x + 1
+        local y = var3 - self.y + 1
+
+        if not self.texture[x] then
+          self.texture[x] = {}
+        end
+        self.texture[x][y] = self.drawColor
+      end
+    end
   end,
 
   needs = {
