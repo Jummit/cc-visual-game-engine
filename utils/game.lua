baf9e71ba776664ff9148f66a93a4154800c622f
Jummit <jummit@web.de>
2018-12-10 13:39:54 +0100

    make game update at regular intervals

--- utils/game.lua
+++ utils/game.lua
@@ -20,6 +20,9 @@ end
 
 function game.update(entities)
   local event, var1, var2, var3 = os.pullEvent()
+  if event == "timer" then
+    os.startTimer(0)
+  end
   for _, entity in ipairs(entities) do
     local entityVars = entityUtils.getVars(entity)
     for _, component in ipairs(entity.components) do
@@ -33,6 +36,8 @@ function game.run(gameEntities)
   local gameTerm = window.create(term.current(), 1, 1, term.getSize())
   local oldTerm = term.redirect(gameTerm)
   entities = gameEntities
+
+  os.startTimer(0)
   while true do
     gameTerm.setVisible(false)
     game.render(e)
