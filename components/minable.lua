ea0172fdbe1b32f1c4b2256577ba7e3476a3a0b1
Jummit <jummit@web.de>
2019-10-24 15:40:38 +0200

    placing

--- components/minable.lua
+++ components/minable.lua
@@ -12,18 +12,25 @@ return {
 		if event == "mouse_click" or event == "mouse_drag" then
 			local tx, ty = math.floor(var2 - cameraX), math.floor(var3 - cameraY)
 			local tile = self.tiles[tx][ty]
-			if tile == 1 then
-				return
-			end
 			local inventory = entityUtils.findEntityWithComponent(entities, "inventory")
-			local item = {
-					name = tostring(tile),
-					amount = 1,
-					texture = self.tileset[tile]}
-			inventoryUtils.insert(inventory, item)
 			
-			self.tiles[tx][ty] = 1
-			self.shape[tx][ty] = false
+			if inventory.holding.amount and tile == 1 then
+				self.tiles[tx][ty] = tonumber(inventory.holding.name)
+				self.shape[tx][ty] = true
+				inventory.holding.amount = inventory.holding.amount - 1
+				if inventory.holding.amount == 0 then
+					inventory.holding = {}
+				end
+			elseif tile ~= 1 then
+				local item = {
+						name = tostring(tile),
+						amount = 1,
+						texture = self.tileset[tile]}
+				inventoryUtils.insert(inventory, item)
+				
+				self.tiles[tx][ty] = 1
+				self.shape[tx][ty] = false
+			end
 		end
 	end,
 	editor = function(self, event, var1, var2, var3, keyboard)
