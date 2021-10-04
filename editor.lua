977366b048d8fbe4dec5a8a8260a0402c31e8efd
Jummit <jummit@web.de>
2021-10-04 19:25:24 +0200

    Don't put many assignments on single line

--- editor.lua
+++ editor.lua
@@ -24,6 +24,11 @@ return function(save)
             self.componentList = ui.list({
                     x = 2, y = entityListHeight + 4,
                     w = sideBarWidth - 2, h = componentListHeight,
+            self.componentList = ui.list{
+                    x = 2,
+                    y = entityListHeight + 4,
+                    w = sideBarWidth - 2,
+                    h = componentListHeight,
                     items = {},
                     getLabel = function(item)
                         return item.type
@@ -57,10 +62,12 @@ return function(save)
                                 type = componentType,
                                 args = tableUtils.copy(components[componentType].args),
                                 needs = components[componentType].needs})
-                    end})
-            self.entityList = ui.list({
-                    x = 2, y = 2,
-                    w = sideBarWidth - 2, h = entityListHeight,
+                    end}
+            self.entityList = ui.list{
+                    x = 2,
+                    y = 2,
+                    w = sideBarWidth - 2,
+                    h = entityListHeight,
                     items = self.runtime.entities,
                     getLabel = function(item)
                         return item.name
@@ -77,30 +84,40 @@ return function(save)
                         if #newName > 0 then
                             self.entityList:getSelected().name = newName
                         end
-                    end})
+                    end}
             self.uiElements = {
                 ui.box{
-                        x = 1, y = 1,
-                        w = sideBarWidth, h = h,
+                        x = 1,
+                        y = 1,
+                        w = sideBarWidth,
+                        h = h,
                         color = colors.lightGray},
                 ui.box{
-                        x = 1, y = h,
-                        w = w, h = 1,
+                        x = 1,
+                        y = h,
+                        w = w,
+                        h = 1,
                         color = colors.lightGray},
                 self.entityList,
                 self.componentList,
                 ui.centerText{
-                        x = 2, y = entityListHeight + 2,
-                        w = sideBarWidth - 2, h = 1,
+                        x = 2,
+                        y = entityListHeight + 2,
+                        w = sideBarWidth - 2,
+                        h = 1,
                         text = "Entities",
                         textColor = colors.white, backgroundColor = colors.lightGray},
                         ui.centerText{
-                        x = 2, y = entityListHeight + componentListHeight + 4,
-                        w = sideBarWidth - 2, h = 1,
-                        text = "Components",
-                        textColor = colors.white, backgroundColor = colors.lightGray},
+                            x = 2,
+                            y = entityListHeight + componentListHeight + 4,
+                            w = sideBarWidth - 2,
+                            h = 1,
+                            text = "Components",
+                            textColor = colors.white, backgroundColor = colors.lightGray
+                        },
                 ui.buttons.addAndDelete{
-                        x = 2, y = entityListHeight + 1,
+                        x = 2,
+                        y = entityListHeight + 1,
                         del = function()
                             self.componentList.items = {}
                             self.entityList:removeSelected()
@@ -112,7 +129,8 @@ return function(save)
                             })
                         end},
                 ui.buttons.addAndDelete{
-                        x = 2, y = entityListHeight + componentListHeight + 3,
+                        x = 2,
+                        y = entityListHeight + componentListHeight + 3,
                         del = function()
                             self.componentList:removeSelected()
                         end,
@@ -120,24 +138,32 @@ return function(save)
                             self.window = ui.newComponentWindow(self.componentList)
                         end},
                 ui.buttons.move{
-                        x = 6, y = entityListHeight + 1,
+                        x = 6,
+                        y = entityListHeight + 1,
                         list = self.entityList},
                 ui.buttons.move{
                         x = 6, y = entityListHeight + componentListHeight + 3,
                         list = self.componentList},
                 ui.button{
-                        x = w - 13, y = h,
-                        w = 4, h = 1,
+                        x = w - 13,
+                        y = h,
+                        w = 4,
+                        h = 1,
                         label = "save",
-                        labelColor = colors.green, color = colors.lime, clickedColor = colors.yellow,
+                        labelColor = colors.green,
+                        color = colors.lime,
+                        clickedColor = colors.yellow,
                         onClick = function()
                             gameSave.save(self.saveFile, self.runtime.entities)
                         end},
                 ui.button{
-                        x = w - 8, y = h,
-                        w = 3, h = 1,
+                        x = w - 8,
+                        y = h,
+                        w = 3,
+                        h = 1,
                         label = "run",
-                        labelColor = colors.blue, color = colors.lightBlue,
+                        labelColor = colors.blue,
+                        color = colors.lightBlue,
                         clickedColor = colors.white,
                         onClick = function()
                             require("utils.log").clear()
@@ -146,20 +172,26 @@ return function(save)
                             self.runtime:run()
                         end},
                 ui.button{
-                        x = w - 5, y = h,
-                        w = 1, h = 1,
+                        x = w - 5,
+                        y = h,
+                        w = 1,
+                        h = 1,
                         label = "-",
-                        labelColor = colors.white, color = colors.gray,
+                        labelColor = colors.white,
+                        color = colors.gray,
                         clickedColor = colors.lightGray,
                         onClick = function(buttonSelf)
                             self.runFullscreen = not self.runFullscreen
                             buttonSelf.label = self.runFullscreen and "+" or "-"
                         end},
                 ui.button{
-                        x = w - 2, y = h,
-                        w = 3, h = 1,
+                        x = w - 2,
+                        y = h,
+                        w = 3,
+                        h = 1,
                         label = "exit",
-                        labelColor = colors.red, color = colors.orange,
+                        labelColor = colors.red,
+                        color = colors.orange,
                         clickedColor = colors.yellow,
                         onClick = function()
                             self.shouldQuit = true
